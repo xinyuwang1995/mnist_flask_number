ad5674b897f57c27336179cd6a450e01d63d173b
xinyuwang1995 <wangxinyu19950915@gmail.com>
2019-11-22 03:42:36 +0800

    mnist

---
+++ mnist/input_data.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division,print_function
+
+import gzip
+import os
+import tempfile
+import numpy
+from six.moves import urllib
+from six.moves import xrange
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
